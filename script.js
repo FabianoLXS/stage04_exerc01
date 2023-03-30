2c7a178147f2c40c02d94029a90afa2b22b0e115
Fabiano Luiz Santos <47800473+FabianoLXS@users.noreply.github.com>
2023-03-30 15:38:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* Bora praticar e rever tudo o que foi ensinado na aula? **💜**
 Nesse desafio, você irá solicitar ao usuário que ele insira **dois números** e, a partir daí, calcular:
 
@@ -54,67 +53,4 @@ if (num1 == num2) {
   alert("números iguais")
 } else {
   alert("números diferentes")
-=======
-/*
-  Crie uma lista de pacientes
-
-  Cada paciente dentro da lista, deverá conter
-    nome
-    idade
-    peso
-    altura
-
-  Escreva uma lista contendo o nome dos pacientes, suas idades, alturas e pesos
-*/
-
-
-/* 
-  Dada uma lista de pacientes, descubra o IMC de cada paciente e imprima
-
-  "Paciente X possui o IMC de: Y"
-
-  Onde X é o nome do paciente e Y é o IMC desse paciente
-
-  Crie uma função para fazer o cálculo de IMC
-*/
-
-/* peso / (altura * altura) */
-
-const patients = [
-  {
-    name: "Luiz",
-    age: 20,
-    weight: 100,
-    height: 190
-  },
-  {
-    name: "Alexandra",
-    age: 27,
-    weight: 70,
-    height: 170
-  },
-  {
-    name: "Carlos",
-    age: 42,
-    weight: 90,
-    height: 180
-  },
-]
-
-//criação da função
-function IMC(weight, height) {
-  return (weight / ((height / 100) ** 2)).toFixed(2)
-}
-
-function printPatientIMC(patient) {
-  return `
-    Paciente ${patient.name} possui o IMC de
-    ${IMC(patient.weight, patient.height)}
-  `
-}
-
-for (let patient of patients) {
-  let IMCmessage = printPatientIMC(patient)
-  alert(IMCmessage)
->>>>>>> efd8fe36ff712d554efd48e68916dbaba06cde46
 }
